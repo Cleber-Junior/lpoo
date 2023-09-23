dd7ced17595a28dfdc01c14d86718a021bc092c1
Cleber San Martins Junior <116321960+Cleber-Junior@users.noreply.github.com>
2023-09-22 23:04:31 -0300

    Add files via upload

---
+++ Objetivo_1/Avaliativo_1/src/Controller/ControllerAluno.java
@@ -0,0 +1,102 @@
+package Controller;
+
+import Model.Aluno;
+
+import java.util.*;
+
+public class ControllerAluno {
+    public static void main(String[] args) {
+        System.out.println("------- EXERCICIO 1 ------- \n");
+
+        // A -
+        System.out.println("Questão A -\n");
+            Aluno a1 = new Aluno(1L, 1122334451, "Jorge", "Santos", "jorge@gmail.com");
+            Aluno a2 = new Aluno(2L, 1234567891, "Hugo", "Da Silva", "hugo@gmail.com");
+            Aluno a3 = new Aluno();
+            Aluno a4 = new Aluno();
+            Aluno a5 = new Aluno(5L, "Ricardo");
+            Aluno a6 = new Aluno(6L, "Gustavo");
+
+            System.out.println(a1);
+            System.out.println(a2);
+            System.out.println(a3);
+            System.out.println(a4);
+            System.out.println(a5);
+            System.out.println(a6);
+
+        // B -
+        System.out.println("Questão B -\n");
+            a3.setId(3L);
+            a3.setCpf(476444821);
+            a3.setNome("Guilherme");
+            a3.setSobrenome("Rocha");
+            a3.setEmail("gui44@gmail.com");
+
+            a4.setId(4L);
+            a4.setCpf(757645764);
+            a4.setNome("Gabriel");
+            a4.setSobrenome("Pereira");
+            a4.setEmail("pereira@gmail.com");
+
+            Long id_3 = a3.getId();
+            int cpf_3 = a3.getCpf();
+            String nome_3 = a3.getNome();
+            String sobrenome_3 = a3.getSobrenome();
+            String email_3 = a3.getEmail();
+
+            Long id_4 = a4.getId();
+            int cpf_4 = a4.getCpf();
+            String nome_4 = a4.getNome();
+            String sobrenome_4 = a4.getSobrenome();
+            String email_4 = a4.getEmail();
+
+            System.out.println("Id - " + id_3);
+            System.out.println("Cpf - " + cpf_3);
+            System.out.println("Nome - " + nome_3);
+            System.out.println("Sobrenome - " + sobrenome_3);
+            System.out.println("Email - " + email_3);
+
+            System.out.println();
+            System.out.println("Id - " + id_4);
+            System.out.println("Cpf - " + cpf_4);
+            System.out.println("Nome - " + nome_4);
+            System.out.println("Sobrenome - " + sobrenome_4);
+            System.out.println("Email - " + email_4);
+
+        // 2 -
+        System.out.println("------- EXECICIO 2 ------- \n");
+
+        List<Aluno> alunos = new ArrayList<>();
+        alunos.add(a1);
+        alunos.add(a2);
+        alunos.add(a3);
+        alunos.add(a4);
+        alunos.add(a5);
+        alunos.add(a6);
+        System.out.println("\n ----- Alunos List -----");
+        System.out.println(alunos);
+
+        System.out.println("\n ----- Alunos Ordem Decrescente -----");
+        alunos.sort(Comparator.comparing(Aluno::getId).reversed());
+        System.out.println(alunos);
+
+        System.out.println("\n ----- Id 5 na Lista -----");
+        System.out.println(alunos.stream().filter(aluno -> aluno.getId()==5).findAny().orElse(null));
+
+        Map<Long, Aluno> AlunosMap = new HashMap<>();
+        AlunosMap.put(a1.getId(), a1);
+        AlunosMap.put(a2.getId(), a2);
+        AlunosMap.put(a3.getId(), a3);
+        AlunosMap.put(a4.getId(), a4);
+        AlunosMap.put(a5.getId(), a5);
+        AlunosMap.put(a6.getId(), a6);
+
+        System.out.println("\n ----- Alunos Map -----");
+        System.out.println(AlunosMap);
+
+        System.out.println("\n ----- Id 5 no Map -----");
+        System.out.println(AlunosMap.get(a5.getId()));
+
+        System.out.println("----- Não é possivel mudar a ordem do Map -----");
+    }
+}
---
+++ Objetivo_1/Lista_02/src/Model/Locacao.java
@@ -0,0 +1,112 @@
+package Model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+
+public class Locacao {
+
+    private Date dataLocacao;
+    private Time horaLocacao;
+    private Date DataDevolucao;
+    private Time horaDevolucao;
+    private Integer quilometragem;
+    private Double calorCalcao;
+    private Double valor_locacao;
+    private Boolean devolvido;
+
+    public Locacao(){
+    }
+
+    public Locacao(Date dataLocacao, Time horaLocacao,
+                   Date dataDevolucao, Time horaDevolucao,
+                   Integer quilometragem, Double calorCalcao,
+                   Double valor_locacao, Boolean devolvido) {
+        this.dataLocacao = dataLocacao;
+        this.horaLocacao = horaLocacao;
+        DataDevolucao = dataDevolucao;
+        this.horaDevolucao = horaDevolucao;
+        this.quilometragem = quilometragem;
+        this.calorCalcao = calorCalcao;
+        this.valor_locacao = valor_locacao;
+        this.devolvido = devolvido;
+    }
+
+    public Date getDataLocacao() {
+        return dataLocacao;
+    }
+
+    public void setDataLocacao(Date dataLocacao) {
+        this.dataLocacao = dataLocacao;
+    }
+
+    public Time getHoraLocacao() {
+        return horaLocacao;
+    }
+
+    public void setHoraLocacao(Time horaLocacao) {
+        this.horaLocacao = horaLocacao;
+    }
+
+    public Date getDataDevolucao() {
+        return DataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        DataDevolucao = dataDevolucao;
+    }
+
+    public Time getHoraDevolucao() {
+        return horaDevolucao;
+    }
+
+    public void setHoraDevolucao(Time horaDevolucao) {
+        this.horaDevolucao = horaDevolucao;
+    }
+
+    public Integer getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(Integer quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public Double getCalorCalcao() {
+        return calorCalcao;
+    }
+
+    public void setCalorCalcao(Double calorCalcao) {
+        this.calorCalcao = calorCalcao;
+    }
+
+    public Double getValor_locacao() {
+        return valor_locacao;
+    }
+
+    public void setValor_locacao(Double valor_locacao) {
+        this.valor_locacao = valor_locacao;
+    }
+
+    public Boolean getDevolvido() {
+        return devolvido;
+    }
+
+    public void setDevolvido(Boolean devolvido) {
+        this.devolvido = devolvido;
+    }
+
+    @Override
+    public String toString() {
+        return "Locacao{" +
+                "dataLocacao=" + dataLocacao +
+                ", horaLocacao=" + horaLocacao +
+                ", DataDevolucao=" + DataDevolucao +
+                ", horaDevolucao=" + horaDevolucao +
+                ", quilometragem=" + quilometragem +
+                ", calorCalcao=" + calorCalcao +
+                ", valor_locacao=" + valor_locacao +
+                ", devolvido=" + devolvido +
+                '}';
+    }
+}
